a82492e071d8637acd925aefa4558d8b22704a14
Suhyeon Jeong <doqtqu@gmail.com>
2022-03-30 14:36:42 +0900

    2022.03.30.
    [feat]
    - 개인정보 제공 테이블 수정 및 expand 가능하게 구조 수정

--- pages/pim/pc.tsx
+++ pages/pim/pc.tsx
@@ -1,8 +1,24 @@
+import { Tabs } from "antd";
+import styled from "styled-components";
+import { ConsignmentTable, ProvisionTable } from "../../components/PCTable";
+
+const { TabPane } = Tabs;
+
+const CustomTabPane = styled(TabPane)`
+  margin-top: 2rem;
+`;
 const Page = () => {
   return (
-    <div>
-      <h2>PC page</h2>
-    </div>
+    <>
+      <Tabs defaultActiveKey="1">
+        <CustomTabPane tab="개인정보 제공" key="1">
+          <ProvisionTable />
+        </CustomTabPane>
+        <CustomTabPane tab="개인정보 위탁" key="2">
+          <ConsignmentTable />
+        </CustomTabPane>
+      </Tabs>
+    </>
   )
 }
 
