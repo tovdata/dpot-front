5b7db34862f86136bc4ce1ec81d4eb30445c344e
hmin01 <goals1003@gmail.com>
2022-04-04 10:53:28 +0900

    2022.04.04. Modify a deploy script for CI/CD

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 # Find a process id (for next.js)
-pid=$(lsof -t -i:3000)
+pid=$(lsof -t -i TCP:3000 -a -c node)
 # If exist a process, kill a process
-if [ -n $pid ]; then
+if [ $pid ]; then
   kill -9 $pid
 fi
