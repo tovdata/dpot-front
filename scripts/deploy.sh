477b39a5d81b85a099ce5f74201ce1070e51bdf9
hmin01 <goals1003@gmail.com>
2022-04-04 12:16:14 +0900

    2022.04.04. Modify a deploy script for CI/CD

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,8 @@ pid=$(lsof -t -i TCP:3000 -a -c node)
 # If exist a process, kill a process
 if [ $pid ]; then
   kill -9 $pid
+  echo "Kill a previous process ($pid)"
 fi
 # Start
 JENKINS_NODE_COOKIE=dontKillMe && nohup npm run start &
+echo "Start a process for front server"
